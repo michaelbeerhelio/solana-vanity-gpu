4eb2ba35e366f25e7438bfed40d4b08e647ddd78
00MB <michaelwbeer@gmail.com>
2024-11-19 13:07:44 +0000

    aoeu

--- src/cuda-ecc-ed25519/gpu_ctx.h
+++ src/cuda-ecc-ed25519/gpu_ctx.h
@@ -30,7 +30,21 @@ extern int32_t g_total_gpus;
 bool cuda_crypt_init();
 bool ed25519_init();
 gpu_ctx_t* get_gpu_ctx();
-bool setup_gpu_ctx();
+void setup_gpu_ctx(verify_ctx_t* cur_ctx,
+                   const gpu_Elems* elems,
+                   uint32_t num_elems,
+                   uint32_t message_size,
+                   uint32_t total_packets,
+                   uint32_t total_signatures,
+                   const uint32_t* message_lens,
+                   const uint32_t* public_key_offsets,
+                   const uint32_t* private_key_offsets,
+                   const uint32_t* signature_offsets,
+                   const uint32_t* message_start_offsets,
+                   uint32_t* out,
+                   size_t packets_size_bytes,
+                   size_t out_size_bytes,
+                   size_t offsets_len);
 void release_gpu_ctx();
 
 #endif
